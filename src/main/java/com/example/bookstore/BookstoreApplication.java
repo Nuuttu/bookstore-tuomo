b3a0b7a956d3a2704206535de6adf69e038d30b5
unknown <miettinen1tuomo@gmail.com>
2021-10-18 16:43:44 +0300

    pom

--- src/main/java/com/example/bookstore/BookstoreApplication.java
+++ src/main/java/com/example/bookstore/BookstoreApplication.java
@@ -27,19 +27,19 @@ public class BookstoreApplication {
 	@Bean
 	public CommandLineRunner bookDemo(BookRepository bookrepo, CategoryRepository catrepo, UserRepository urepository) {
 		return (args) -> {
-			log.info("save a couple of categories and books");
-			catrepo.save(new Category("Thriller"));
-			catrepo.save(new Category("Fantasy"));
-			catrepo.save(new Category("Scifi"));
+			//log.info("save a couple of categories and books");
+			//catrepo.save(new Category("Thriller"));
+			//catrepo.save(new Category("Fantasy"));
+			//catrepo.save(new Category("Scifi"));
 			
-			bookrepo.save(new Book("Book2", "Kateson", 1992, 29299292, 20.2, catrepo.findByName("Thriller").get(0)));
-			bookrepo.save(new Book("Book1", "Johnson", 1990, 1293129393, 20.1, catrepo.findByName("Scifi").get(0)));
+			//bookrepo.save(new Book("Book2", "Kateson", 1992, 29299292, 20.2, catrepo.findByName("Thriller").get(0)));
+			//bookrepo.save(new Book("Book1", "Johnson", 1990, 1293129393, 20.1, catrepo.findByName("Scifi").get(0)));
 			
       			// Create users: admin/admin user/user
-			User user1 = new User("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER");
-			User user2 = new User("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "ADMIN");
-			urepository.save(user1);
-			urepository.save(user2);
+			//User user1 = new User("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER");
+			//User user2 = new User("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "ADMIN");
+			//urepository.save(user1);
+			//urepository.save(user2);
 			
 			log.info("fetch all books");
 			for (Book book: bookrepo.findAll()) {
